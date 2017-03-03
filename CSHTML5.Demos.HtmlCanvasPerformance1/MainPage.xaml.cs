1e6da1466505e679c4a98febf85adcfdff260dd9
Giovanni <Giovanni@WIN8-MPB17>
2017-03-03 14:33:26 +0100

    Add project files.

---
+++ CSHTML5.Demos.HtmlCanvasPerformance1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CSHTML5.Demos.HtmlCanvasPerformance1
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+        }
+    }
+}
